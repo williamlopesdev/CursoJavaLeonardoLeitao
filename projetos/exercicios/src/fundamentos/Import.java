76453ae4645eed2fbeb47d02e25503eb1b0e8e32
williamlopesdev <wilcypherr@gmail.com>
2024-01-06 16:52:46 -0300

    Tipo String

---
+++ projetos/exercicios/src/fundamentos/Import.java
@@ -0,0 +1,18 @@
+
+
+package fundamentos;
+
+import java.util.Date;
+
+import javax.swing.JButton;
+
+public class Import {
+	public static void main(String[] args) {
+		
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+		JButton  botao = new JButton();
+	}
+}
